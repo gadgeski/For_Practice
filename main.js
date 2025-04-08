0e4d294b9a6f58fa1a93754c1a3f1c270e72188a
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-08 22:05:55 +0900

    スペルミスを修正

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const interests = ["ホヨバゲー","ガジェット","プログラミング"]；
-const specialities = ["ちなこの設問は","めっちゃ迷ってる","どないすんねん？"]；
+const specialties = ["ちなこの設問は","めっちゃ迷ってる","どないすんねん？"]；
 const greetings = ["トマトマゼール？","パスタクエール？","バイトコレール？"]
 const button = document.querySelector("#button");
 button.addEventListener("click", function () {
     const name = document.querySelector("name").value;
     const index1 = Math.floor(Math.random() * interests.length);
-    const index2 = Math.floor(Math.random() * specialities.length);
+    const index2 = Math.floor(Math.random() * specialties.length);
     const index3 = Math.floor(Math.random() * greetings.length);
     const interest = interests[index1];
-    const speciality = specialities[index2];
+    const specialty = specialties[index2];
     const greeting = greetings[index3];
-    const message = 'こんちくわ $[name]さん
-    最近ハマっているのは$[interrest]で、ぶっちゃけ$[speciality]なんだけど$[greeting]';
+    const message = 'こんちくわ $[name]さん。
+    最近ハマっているのは$[interest]で、ぶっちゃけ$[specialty]なんだけど$[greeting]';
     const output = document.querySelector("#output");
     output.textContent = message;
 });
