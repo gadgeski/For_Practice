b66f75c5d29fb7637699f65ec0db713562ca1246
gadgeski <xtwh.ce5bil@gmail.com>
2025-04-08 12:47:50 +0900

    単語リスト追加

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+const interests = ["ホヨバゲー","ガジェット","プログラミング"]；
+const specialities = ["ちなこの設問は","めっちゃ迷ってる","どないすんねん？"]；
+const greetings = ["トマトマゼール？","パスタクエール？","バイトコレール？"]
 const button = document.querySelector("#button");
 button.addEventListener("click", function () {
     const name = document.querySelector("name").value;
-    const message = 'こんちくわ $[name]さん';
+    const index1 = Math.floor(Math.random() * interests.length);
+    const index2 = Math.floor(Math.random() * specialities.length);
+    const index3 = Math.floor(Math.random() * greetings.length);
+    const interest = interests[index1];
+    const speciality = specialities[index2];
+    const greeting = greetings[index3];
+    const message = 'こんちくわ $[name]さん
+    最近ハマっているのは$[interrest]で、ぶっちゃけ$[speciality]なんだけど$[greeting]';
     const output = document.querySelector("#output");
     output.textContent = message;
 });
